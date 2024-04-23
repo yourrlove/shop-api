619a7689945952fe37bc0b98a37f9f95d8c71e3a
yourrlove <nanhvt2708@gmail.com>
2024-04-24 00:40:07 +0700

    refractor: Config constants for production and development evironment

--- src/configs/config.mysql.js
+++ src/configs/config.mysql.js
@@ -20,11 +20,15 @@ module.exports = {
         "dialect": "mysql"
     },
     "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.MYSQL_DB_HOST,
+        "port": process.env.MYSQL_DB_PORT,
+        "dialect": "mysql",
+        "define": {
+            freezeTableName: true,
+        }
     },
 };
 
