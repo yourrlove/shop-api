81253fc1c3c7f05af0784c1b653a37d91520f503
yourrlove <nanhvt2708@gmail.com>
2024-04-28 12:07:37 +0700

    fix: cors

--- src/middlewares/credentials.js
+++ src/middlewares/credentials.js
@@ -3,7 +3,6 @@ const allowedOrigins = require('../configs/CORS/allowedOrigins');
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.
         res.header('Access-Control-Allow-Credentials', true);
     }
     next();
