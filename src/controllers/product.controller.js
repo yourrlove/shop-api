dd9140bdb8af0274d6ed6d045238b199f6f3cd8c
yourrlove <nanhvt2708@gmail.com>
2024-04-28 20:17:17 +0700

    fix: fix cart & product controller, cart_item service, cart model #8

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -102,68 +102,7 @@ class ProductController {
             message: 'List of products',
             metadata: await ProductService.filter_by_query_options(req.body)
         }).send(res);
-    }
-
-    get_list_products = async (req, res, next) => {
-        try {
-            res.status(200).json(await ProductService.get_all());
-        }
-        catch (err) {
-            res.status(500).json(err);
-        }
-    }
-
-    update_product = async (req, res, next) => {
-        try {
-            res.status(200).json(await ProductService.update(req.params.id, req.body));
-        }
-        catch (err) {
-            res.status(500).json(err);
-        }
-    }
-
-    delete_product = async (req, res, next) => {
-        try {
-            res.status(200).json(await ProductService.delete(req.params.id));
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    }
-
-    create_product_detail = async (req, res, next) => {
-        try {
-            res.status(201).json(await ProductDetailService.create(req.params.id, req.body));
-        }
-        catch (err) {
-            res.status(500).json(err);
-        }
-    }
-
-    get_all_product_details = async (req, res, next) => {
-        try {
-            res.status(200).json(await ProductDetailService.get_all(req.params.id));
-        }
-        catch (err) {
-            res.status(500).json(err);
-        }
-    }
-    
-    delete_product_detail = async (req, res, next) => {
-        try {
-            res.status(200).json(await ProductDetailService.delete(req.params.id));
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    }
-
-    update_product_detail = async (req, res, next) => {
-        try {
-            res.status(200).json(await ProductDetailService.update(req.params.id, req.body));
-        } catch (err) {
-            res.status(500).json(err);
-        }
-    }
-    
+    }    
 }
 
 module.exports = new ProductController();
