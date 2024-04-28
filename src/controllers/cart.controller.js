3067c84feecb603c7cf579186e97781c626c71df
Dang Thanh Dat <dangthanhdatuit@gmail.com>
2024-04-28 17:39:35 +0700

    Cart and cartItem

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -5,75 +5,52 @@ const { OK, CREATED } = require('../core/success.response');
 
 class CartController {
     get_list_carts = async(req, res, next) => {
-        try{
-            new OK ({
-                message: "List users' carts",
-                metadata: await CartService.get_all()
-            }).send(res)
-        }catch(err){
-            res.status(500).json(err);
-        }
+        new OK ({
+            message: "List users' carts",
+            metadata: await CartService.get_all()
+        }).send(res)
     }
     
     add_to_cart = async (req, res, next) => {
-        console.log(req.body)
-        try {
-            new CREATED ({
-                message: "CartItem is added successfully",
-                metadata: await await CartItemService.create(req.params.id, req.body)
-            }).send(res)
-        } catch (err) {
-            res.status(500).json({ error: err.message || "Internal Server Error" });
-        }
+        new CREATED ({
+            message: "CartItem is added successfully",
+            metadata: await await CartItemService.create(req.params.id, req.body)
+        }).send(res)
     }
 
     create_cart = async (req, res, next) => {
-        debugger;
-        try {
-            const newCart = await CartService.create(req.body);
-            new CREATED({
-                message: "Cart created successfully",
-                metadata: newCart
-            }).send(res);
-        } catch (err) {
-            res.status(500).json({ error: err.message || "Internal Server Error" });
-        }
+        const newCart = await CartService.create(req.body);
+        new CREATED({
+            message: "Cart created successfully",
+            metadata: newCart
+        }).send(res);
     }
 
     update_cart = async (req, res, next) => {
-        try {
-            const updatedItem = await CartItemService.updateQuantity(req.params.id, req.body);
-            new OK({
-                message: "Cart updated successfully",
-                metadata: updatedItem
-            }).send(res);
-        } catch (err) {
-            res.status(500).json({ error: err.message || "Internal Server Error" });
-        }
+        
+        const updatedItem = await CartItemService.updateQuantity(req.params.id, req.body);
+        new OK({
+            message: "Cart updated successfully",
+            metadata: updatedItem
+        }).send(res);
+        
     }
 
     delete_cart_item = async (req, res, next) => {
-        try {
-            const deletedItem = await CartItemService.delete(req.params.id, req.body);
-            new OK({
-                message: "Cart item deleted successfully",
-                metadata: deletedItem
-            }).send(res);
-        } catch (err) {
-            res.status(500).json({ error: err.message || "Internal Server Error" });
-        }
+        
+        const deletedItem = await CartItemService.delete(req.params.id, req.body);
+        new OK({
+            message: "Cart item deleted successfully",
+            metadata: deletedItem
+        }).send(res);  
     }
 
     get_list_item = async (req, res, next) => {
-        try {
-            const items = await CartItemService.get_all(req.params.id);
-            new OK({
-                message: "List items in cart",
-                metadata: items
-            }).send(res);
-        } catch (err) {
-            res.status(500).json({ error: err.message || "Internal Server Error" });
-        }
+        const items = await CartItemService.get_all(req.params.id);
+        new OK({
+            message: "List items in cart",
+            metadata: items
+        }).send(res);
     }
 }
 
