609899ece6b6d92bfc9d2c88cf3eba4b1a6df8ec
yourrlove <nanhvt2708@gmail.com>
2024-05-05 21:54:53 +0700

    fix: fix logout, remove verify accesstoken #48

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,7 +8,7 @@ const { verifyToken } = require('../middlewares/auth');
 
 router.post('/signup', asyncHandler( AuthContoller.signUp ));
 router.post('/login', asyncHandler( AuthContoller.logIn ));
-router.get('/logout', verifyToken, asyncHandler( AuthContoller.logOut ));
+router.get('/logout', asyncHandler( AuthContoller.logOut ));
 router.get('/getaccesstoken', asyncHandler( AuthContoller.handleRefreshToken ));
 
 module.exports = router;
