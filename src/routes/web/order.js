8058a34a592c119a7878918c4535ef00c9e06114
yourrlove <nanhvt2708@gmail.com>
2024-05-20 23:38:58 +0700

    feat: implement checkout endpoint to review delivery information

--- src/routes/web/order.js
+++ src/routes/web/order.js
@@ -6,6 +6,7 @@ const { asyncHandler } = require('../../helpers/index');
 const { verifyToken } = require('../../middlewares/auth');
 const OrderController = require('../../controllers/order.controller');
 
+router.get('/deliveryinfor', verifyToken, asyncHandler( OrderController.checkOutDeliveryInformation ));
 router.post('/checkout', verifyToken, asyncHandler( OrderController.checkOutReviewOrder ));
 router.post('/', verifyToken, asyncHandler( OrderController.createOrder ));
 
